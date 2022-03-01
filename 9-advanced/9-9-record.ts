a299d76a006cd064b9128c6775b154b2ee1e8414
autumnly1007 <autumnly1007@gmail.com>
2022-03-01 22:41:06 +0900

    utility type

--- 9-advanced/9-9-record.ts
+++ 9-advanced/9-9-record.ts
@@ -6,10 +6,13 @@
   type Page = 'home' | 'about' | 'contact';
 
   // Record<Key, Value>
-  // map 과 비슷함. 둘을 연결하고 싶을 경우 사용
+  // map 과 비슷함. 두 개의 타입을 연결하고 싶을 경우 사용
   const nav: Record<Page, PageInfo> = {
     home: { title: 'Home' },
     about: { title: 'About' },
     contact: {title: 'Contact'}
   };
+
+  type Product = 'cat' | 'dog';
+  type NewProduct = Capitalize<Product>; // 'Cat' | 'Dog'
 }
